985770c6957bbc6380bb2c68c3cde5e782a98dcd
Hamza <hamzazubair228@gmail.com>
2024-02-09 22:33:48 +0500

    update

--- src/components/MainPage/MainPage.styles.js
+++ src/components/MainPage/MainPage.styles.js
@@ -1,8 +1,108 @@
 import styled from 'styled-components';
 import Landingbg from '../../../public/Landingbg.jpg';
 export const StyledMainPageStyles = styled.div`
+  position: relative;
   background-image: url(${Landingbg.src});
   min-height: 100vh;
   background-size: cover;
   background-repeat: repeat;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 100px 20px 0 20px;
+  .text {
+    max-width: 542px;
+    width: 100%;
+    text-align: center;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 24px;
+    letter-spacing: 0em;
+    text-align: center;
+    p {
+      margin-bottom: 0;
+    }
+    .home-page {
+      display: block;
+      margin: 20px 0;
+      text-decoration: underline;
+      color: var(--primary-500);
+    }
+    .buttonWrapper {
+      max-width: 280px;
+      width: 100%;
+      margin: 0 auto;
+      button {
+        margin-bottom: 15px;
+        &:nth-last-child() {
+          margin-bottom: 0;
+        }
+      }
+    }
+  }
+  .title {
+    margin-bottom: 25px;
+    .connect,
+    .logo {
+      font-size: 35px;
+      line-height: 24px;
+      font-weight: 300;
+      letter-spacing: 0em;
+      text-align: center;
+      padding-left: 15px;
+      color: var(--primary-500);
+      @media screen and (min-width: 768px) {
+        font-size: 50px;
+      }
+      @media screen and (min-width: 992px) {
+        font-size: 60px;
+      }
+    }
+    .logo {
+      font-weight: 500;
+      padding: 0;
+    }
+  }
+`;
+
+export const HomeFooter = styled.div`
+  padding: 20px;
+  max-width: 1200px;
+  width: 100%;
+  position: absolute;
+  bottom: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 20px;
+  text-align: center;
+  @media screen and (max-width: 600px) {
+    justify-content: center;
+  }
+  span {
+    padding-right: 20px;
+  }
+
+  .socialLinks {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    max-width: 250px;
+    width: 100%;
+    span {
+      padding-right: 20px;
+    }
+    .imageWrapper {
+      flex-shrink: 0;
+      max-width: 26px;
+      cursor: pointer;
+      img {
+        max-width: 100%;
+        height: auto;
+      }
+    }
+  }
 `;
--- src/components/MainPage/index.jsx
+++ src/components/MainPage/index.jsx
@@ -1,8 +1,60 @@
 import React from 'react';
-import { StyledMainPageStyles } from './MainPage.styles';
-
+import { HomeFooter, StyledMainPageStyles } from './MainPage.styles';
+import Link from 'next/link';
+import Button from '../Button';
+import tiktok from '../../../public/tiktok.svg';
+import insta from '../../../public/insta.svg';
+import tweet from '../../../public/tweet.svg';
+import fb from '../../../public/fb.svg';
+import Image from 'next/image';
 const MainPage = () => {
-  return <StyledMainPageStyles>MainPage</StyledMainPageStyles>;
+  return (
+    <>
+      <StyledMainPageStyles>
+        <div className="text">
+          <div className="title">
+            <strong className="logo">LOCKE</strong>
+            <strong className="connect">CONNECT</strong>
+          </div>
+          <p>
+            Connect instantly with local real estate experts, ensuring efficiency and transparency for confident buying
+            and selling experiences.{' '}
+          </p>
+          <p>More than a transaction, it is a connection!</p>
+          <Link href="/" className="home-page">
+            Homepage
+          </Link>
+          <div className="buttonWrapper">
+            <Link href="sign-in">
+              <Button variant="primary">Sign in</Button>
+            </Link>
+            <Link href="sign-up">
+              <Button outline="primary">Register</Button>
+            </Link>
+          </div>
+        </div>
+        <HomeFooter>
+          <span>Terms of use</span>
+          <span>Copyright © LockeConnect</span>
+          <div className="socialLinks">
+            <span>Connect us</span>
+            <div className="imageWrapper">
+              <Image src={fb} alt="fb" />
+            </div>
+            <div className="imageWrapper">
+              <Image src={tweet} alt="tweet" />
+            </div>
+            <div className="imageWrapper">
+              <Image src={insta} alt="insta" />
+            </div>
+            <div className="imageWrapper">
+              <Image src={tiktok} alt="tiktok" />
+            </div>
+          </div>
+        </HomeFooter>
+      </StyledMainPageStyles>
+    </>
+  );
 };
 
 export default MainPage;
