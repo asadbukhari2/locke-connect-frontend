1c6af67a1c34b6196e07065c9b3a38500be79475
ans4asif <ans4asif@gmail.com>
2024-02-11 23:20:44 +0500

    up

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -24,53 +24,54 @@ const Pagination = ({ page, totalCount, totalPages = 0, pageSize, isLoading = fa
     onPageChange(e);
   };
   return (
-    <PaginationHolder className="pagination">
-      <div className="pages-holder">
-        <button onClick={handlePrevPage} disabled={isLoading}>
-          <GrFormPrevious size={25} />
-        </button>
-        <div className="pages">
-          {Array.from({ length: totalPages }).map((_, index) => {
-            if (
-              (typeof window !== 'undefined' && window.innerWidth <= 600 && index < 6) ||
-              (typeof window !== 'undefined' &&
-                window.innerWidth > 600 &&
-                (index < 4 || (index >= currentPage - 2 && index <= currentPage + 2) || index > 10))
-            ) {
-              return (
-                <span
-                  key={index}
-                  onClick={() => {
-                    setCurrentPage(index + 1);
-                    handleCurrentPage(index + 1);
-                  }}
-                  className={currentPage === index + 1 ? 'active' : ''}>
-                  {index + 1}
-                </span>
-              );
-            } else if (typeof window !== 'undefined' && window.innerWidth > 600 && index === 6) {
-              return <span key={index}>...</span>;
-            } else if (typeof window !== 'undefined' && window.innerWidth > 600 && index === 10) {
-              return (
-                <>
-                  <div key={index - 1} onClick={() => setCurrentPage(10)}>
-                    {10}
-                  </div>
-                  <span key={index} onClick={() => setCurrentPage(11)} className={currentPage === 11 ? 'active' : ''}>
-                    {11}
-                  </span>
-                </>
-              );
-            }
-            return null;
-          })}
-        </div>
+    <></>
+    // <PaginationHolder className="pagination">
+    //   <div className="pages-holder">
+    //     <button onClick={handlePrevPage} disabled={isLoading}>
+    //       <GrFormPrevious size={25} />
+    //     </button>
+    //     <div className="pages">
+    //       {Array.from({ length: totalPages }).map((_, index) => {
+    //         if (
+    //           (typeof window !== 'undefined' && window.innerWidth <= 600 && index < 6) ||
+    //           (typeof window !== 'undefined' &&
+    //             window.innerWidth > 600 &&
+    //             (index < 4 || (index >= currentPage - 2 && index <= currentPage + 2) || index > 10))
+    //         ) {
+    //           return (
+    //             <span
+    //               key={index}
+    //               onClick={() => {
+    //                 setCurrentPage(index + 1);
+    //                 handleCurrentPage(index + 1);
+    //               }}
+    //               className={currentPage === index + 1 ? 'active' : ''}>
+    //               {index + 1}
+    //             </span>
+    //           );
+    //         } else if (typeof window !== 'undefined' && window.innerWidth > 600 && index === 6) {
+    //           return <span key={index}>...</span>;
+    //         } else if (typeof window !== 'undefined' && window.innerWidth > 600 && index === 10) {
+    //           return (
+    //             <>
+    //               <div key={index - 1} onClick={() => setCurrentPage(10)}>
+    //                 {10}
+    //               </div>
+    //               <span key={index} onClick={() => setCurrentPage(11)} className={currentPage === 11 ? 'active' : ''}>
+    //                 {11}
+    //               </span>
+    //             </>
+    //           );
+    //         }
+    //         return null;
+    //       })}
+    //     </div>
 
-        <button onClick={handleNextPage} disabled={isLoading}>
-          <GrFormNext size={25} />
-        </button>
-      </div>
-    </PaginationHolder>
+    //     <button onClick={handleNextPage} disabled={isLoading}>
+    //       <GrFormNext size={25} />
+    //     </button>
+    //   </div>
+    // </PaginationHolder>
   );
 };
 
